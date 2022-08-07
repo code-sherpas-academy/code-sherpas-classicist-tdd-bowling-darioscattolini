dc2afb48ea54ddb39dccbc9ec5d1fce79c93f64f
Darío Scattolini <dario.scattolini@gmail.com>
2022-08-07 15:34:26 +0200

    add test: strike in any frame, pins knocked down in both attempts of next frame

--- lib/src/test/kotlin/sample/CalculateScoreTest.kt
+++ lib/src/test/kotlin/sample/CalculateScoreTest.kt
@@ -41,6 +41,7 @@ class CalculateScoreTest {
         "X|26|--|--|--|--|--|--|--|--||, 26",
         "X|X|--|--|--|--|--|--|--|--||, 30",
         "X|X|31|--|--|--|--|--|--|--||, 41",
+        "--|--|X|71|--|--|--|--|--|--||, 28",
     )
     fun `returns 10 + pins knocked down in next two attempts for any frame with strike`(input: String, expectedScore: Int) {
         assertThat(calculateScore(input)).isEqualTo(expectedScore)
